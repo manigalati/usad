fe75b12fb09435c587152d7710eb48d58544a725
manigalati <32110860+manigalati@users.noreply.github.com>
2020-08-07 15:21:08 +0200

    Add files via upload

---
+++ usad.py
@@ -0,0 +1,101 @@
+class Encoder(nn.Module):
+  def __init__(self, in_size, latent_size):
+    super().__init__()
+    self.linear1 = nn.Linear(in_size, int(in_size/2))
+    self.linear2 = nn.Linear(int(in_size/2), int(in_size/4))
+    self.linear3 = nn.Linear(int(in_size/4), latent_size)
+    self.relu = nn.ReLU(True)
+        
+  def forward(self, w):
+    out = self.linear1(w)
+    out = self.relu(out)
+    out = self.linear2(out)
+    out = self.relu(out)
+    out = self.linear3(out)
+    z = self.relu(out)
+    return z
+    
+class Decoder(nn.Module):
+  def __init__(self, latent_size, out_size):
+    super().__init__()
+    self.linear1 = nn.Linear(latent_size, int(out_size/4))
+    self.linear2 = nn.Linear(int(out_size/4), int(out_size/2))
+    self.linear3 = nn.Linear(int(out_size/2), out_size)
+    self.relu = nn.ReLU(True)
+    self.sigmoid = nn.Sigmoid()
+        
+  def forward(self, z):
+    out = self.linear1(z)
+    out = self.relu(out)
+    out = self.linear2(out)
+    out = self.relu(out)
+    out = self.linear3(out)
+    w = self.sigmoid(out)
+    return w
+    
+class UsadModel(nn.Module):
+  def __init__(self, w_size, z_size):
+    super().__init__()
+    self.encoder = Encoder(w_size, z_size)
+    self.decoder1 = Decoder(z_size, w_size)
+    self.decoder2 = Decoder(z_size, w_size)
+  
+  def training_step(self, batch, n):
+    z = self.encoder(batch)
+    w1 = self.decoder1(z)
+    w2 = self.decoder2(z)
+    w3 = self.decoder2(self.encoder(w1))
+    loss1 = 1/n*torch.mean((batch-w1)**2)+(1-1/n)*torch.mean((batch-w3)**2)
+    loss2 = 1/n*torch.mean((batch-w2)**2)-(1-1/n)*torch.mean((batch-w3)**2)
+    return loss1,loss2
+
+  def validation_step(self, batch, n):
+    z = self.encoder(batch)
+    w1 = self.decoder1(z)
+    w2 = self.decoder2(z)
+    w3 = self.decoder2(self.encoder(w1))
+    loss1 = 1/n*torch.mean((batch-w1)**2)+(1-1/n)*torch.mean((batch-w3)**2)
+    loss2 = 1/n*torch.mean((batch-w2)**2)-(1-1/n)*torch.mean((batch-w3)**2)
+    return {'val_loss1': loss1, 'val_loss2': loss2}
+        
+  def validation_epoch_end(self, outputs):
+    batch_losses1 = [x['val_loss1'] for x in outputs]
+    epoch_loss1 = torch.stack(batch_losses1).mean()
+    batch_losses2 = [x['val_loss2'] for x in outputs]
+    epoch_loss2 = torch.stack(batch_losses2).mean()
+    return {'val_loss1': epoch_loss1.item(), 'val_loss2': epoch_loss2.item()}
+    
+  def epoch_end(self, epoch, result):
+    print("Epoch [{}], val_loss1: {:.4f}, val_loss2: {:.4f}".format(epoch, result['val_loss1'], result['val_loss2']))
+    
+def evaluate(model, val_loader, n):
+    outputs = [model.validation_step(to_device(batch,device), n) for [batch] in val_loader]
+    return model.validation_epoch_end(outputs)
+
+def training(epochs, model, train_loader, val_loader, opt_func=torch.optim.Adam):
+    history = []
+    optimizer1 = opt_func(list(model.encoder.parameters())+list(model.decoder1.parameters()))
+    optimizer2 = opt_func(list(model.encoder.parameters())+list(model.decoder2.parameters()))
+    for epoch in range(epochs):
+        for [batch] in train_loader:
+            batch=to_device(batch,device)
+            loss1,loss2 = model.training_step(batch,epoch+1)
+            loss1.backward(retain_graph=True)
+            loss2.backward()
+            optimizer1.step()
+            optimizer2.step()
+            optimizer1.zero_grad()
+            optimizer2.zero_grad()
+        result = evaluate(model, val_loader, epoch+1)
+        model.epoch_end(epoch, result)
+        history.append(result)
+    return history
+    
+def testing(model, test_loader, alpha=.5, beta=.5):
+    results=[]
+    for [batch] in test_loader:
+        batch=to_device(batch,device)
+        w1=model.decoder1(model.encoder(batch))
+        w2=model.decoder2(model.encoder(w1))
+        results.append(alpha*torch.mean((batch-w1)**2,axis=1)+beta*torch.mean((batch-w2)**2,axis=1))
+    return results
---
+++ utils.py
@@ -0,0 +1,53 @@
+def get_default_device():
+    """Pick GPU if available, else CPU"""
+    if torch.cuda.is_available():
+        return torch.device('cuda')
+    else:
+        return torch.device('cpu')
+
+def to_device(data, device):
+    """Move tensor(s) to chosen device"""
+    if isinstance(data, (list,tuple)):
+        return [to_device(x, device) for x in data]
+    return data.to(device, non_blocking=True)
+    
+def plot_history(history):
+    losses1 = [x['val_loss1'] for x in history]
+    losses2 = [x['val_loss2'] for x in history]
+    plt.plot(losses1, '-x', label="loss1")
+    plt.plot(losses2, '-x', label="loss2")
+    plt.xlabel('epoch')
+    plt.ylabel('loss')
+    plt.legend()
+    plt.title('Losses vs. No. of epochs')
+    plt.grid()
+    plt.show()
+    
+def ROC(y_test,y_pred):
+    fpr,tpr,tr=roc_curve(y_test,y_pred)
+    auc=roc_auc_score(y_test,y_pred)
+    idx=np.argwhere(np.diff(np.sign(tpr-(1-fpr)))).flatten()
+
+    plt.xlabel("FPR")
+    plt.ylabel("TPR")
+    plt.plot(fpr,tpr,label="AUC="+str(auc))
+    plt.plot(fpr,1-fpr,'r:')
+    plt.plot(fpr[idx],tpr[idx], 'ro')
+    plt.legend(loc=4)
+    plt.grid()
+    plt.show()
+    return tr[idx]
+    
+def confusion_matrix(target, predicted, perc=False):
+
+    data = {'y_Actual':    target,
+            'y_Predicted': predicted
+            }
+    df = pd.DataFrame(data, columns=['y_Predicted','y_Actual'])
+    confusion_matrix = pd.crosstab(df['y_Predicted'], df['y_Actual'], rownames=['Predicted'], colnames=['Actual'])
+    
+    if perc:
+        sns.heatmap(confusion_matrix/np.sum(confusion_matrix), annot=True, fmt='.2%', cmap='Blues')
+    else:
+        sns.heatmap(confusion_matrix, annot=True, fmt='d')
+    plt.show()
